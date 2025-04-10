d46f82e7f7c1afe132a2c6893ff43aefca846f75
Edward Marecos <emarecos@bu.edu>
2025-04-10 14:51:26 -0400

    update from my other pc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from "next";
 import React from "react";
+import Header from "@/components/Header";
+
+export const metadata: Metadata = {
+    title: "MP-5 URL Shortener",
+    description: "a URL Shortener application",
+    icons: "https://imgcdn.sigstick.com/iNqRWEt8wH6CF38gOGUS/28-1.png" // cute cat image
+}
 
 export default function RootLayout({
    children,
@@ -7,8 +15,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body style={{'backgroundColor': "beige"}}>
-            {children}
+            <body>
+            <Header />
+                {children}
             </body>
         </html>
     )
